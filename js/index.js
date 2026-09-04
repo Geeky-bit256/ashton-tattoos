867eb2c32c80d1eeaf1f306c7ff6aadde3aecff1
Geeky-bit256 <mohapiwalter716@gmail.com>
2026-09-04 09:24:56 +0200

    Complete Ashton Tattoos  website

--- js/index.js
+++ js/index.js
@@ -2,9 +2,16 @@ const menuToggle = document.querySelector(".menu-toggle");
 const mobileMenu = document.querySelector(".mobile-menu"); 
 
 menuToggle.addEventListener("click", () => { 
-  
   const isOpen = mobileMenu.classList.toggle("is-open"); 
   menuToggle.classList.toggle("is-open");
-  menuToggle.setAttribute( "aria-expanded", isOpen ); 
-  
+  menuToggle.setAttribute("aria-expanded", isOpen); 
 }); 
+
+// Close mobile menu when a link is clicked
+mobileMenu.querySelectorAll("a").forEach(link => {
+  link.addEventListener("click", () => {
+    mobileMenu.classList.remove("is-open");
+    menuToggle.classList.remove("is-open");
+    menuToggle.setAttribute("aria-expanded", "false");
+  });
+});
